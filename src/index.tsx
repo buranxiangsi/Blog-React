edd9c3858a0023019ebd51c81b887bc94c7f0cb5
buranxiangsi <l13789060033@163.com>
2021-10-23 13:40:43 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.scss"
-import { Octokit } from "octokit";
 
-// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: `brxs1-personal-access-token` });
-
-console.log(octokit)
 ReactDOM.render(
     <App />,
   document.getElementById('root')
